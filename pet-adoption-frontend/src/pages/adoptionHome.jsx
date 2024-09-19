fc131f2738b33d8fa2895e93b6996f8f1ba6203c
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-09-19 16:29:39 -0500

    Added a call to the adoption home backend

--- pet-adoption-frontend/src/pages/adoptionHome.jsx
+++ pet-adoption-frontend/src/pages/adoptionHome.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head'
 import { Box, Button, Card, CardContent, Stack, Typography } from '@mui/material'
 
 export default function adoptionHome(){
-    return <h1>Welcome to Adoption Home</h1>
-}
+    const[data, setData] = useState('');
+    useEffect(() =>{ 
+        const fetchData = () =>{
+            fetch("http://localhost:8080/AdoptHome")
+            .then(function(respose){
+                if(!Response.ok){
+                    throw new Error("Bad network response");
+                }
+                return respose.text();
+            })
+            .then(function(result){
+                setData(result);
+            })
+            .catch(function (error) {
+                console.error('Error fetching data:', error);
+            });
+
+
+        };
+
+        fetchData();
+        return <h1>{data}</h1>
+
+    });
+
+
+
+
+    }
+    
+    
+    
+    
+    
+
