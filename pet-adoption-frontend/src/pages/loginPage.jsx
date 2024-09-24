4a6f57472bd9514fccecbe520105a1becff56f74
djm1203 <derekmartinez1203@gmail.com>
2024-09-24 09:00:33 -0500

    Created frontend login page. Need to add visuals

---
+++ pet-adoption-frontend/src/pages/loginPage.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import Head from 'next/head';
+import {Box, Card, CardContent, Typography} from "@mui/material";
+
+export default function LoginPage() {
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/login");
+                if(!response.ok){
+                    throw new Error("Bad network response");
+                }
+                const result = await response.text();
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <Box>
+
+        </Box>
+    );
+}
