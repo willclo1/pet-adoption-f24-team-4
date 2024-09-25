3eee5ea13499b97aa417670f21eb14f1c2f704c4
HarryH20 <harrswag0123@gmail.com>
2024-09-25 15:51:18 -0500

    customer page

--- pet-adoption-frontend/src/pages/index.jsx
+++ pet-adoption-frontend/src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import Head from 'next/head'
 import { Button, Card, CardContent, Stack, Typography } from '@mui/material'
 import styles from '@/styles/Home.module.css'
