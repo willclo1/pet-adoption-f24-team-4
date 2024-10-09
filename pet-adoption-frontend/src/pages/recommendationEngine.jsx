931a275bedabb76ddd22d64688c869635e1f3f8a
djm1203 <derekmartinez1203@gmail.com>
2024-10-09 14:05:02 -0500

    created and added mathching page with a drawer ui, still need to track user.

--- pet-adoption-frontend/src/pages/recommendationEngine.jsx
+++ pet-adoption-frontend/src/pages/recommendationEngine.jsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Stack, Typography, AppBar, Toolbar, Button, Avatar, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Snackbar } from '@mui/material';
+import { Stack, Typography, AppBar, Toolbar, Button, Avatar, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Snackbar, Drawer, ListItem, List, ListItemText, ListItemButton, Box, ListItemIcon} from '@mui/material';
+import HouseIcon from '@mui/icons-material/House';
+import GroupsIcon from '@mui/icons-material/Groups';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import MenuIcon from '@mui/icons-material/Menu';
+import PetsIcon from '@mui/icons-material/Pets';
 import { useRouter } from 'next/router';
 
 export default function recommendationEnginePage() {
 
+  const [state, setState] = React.useState({
+    left: false
+  });
     //const { email } = router.query; // Use email from query parameters
     //const [user, setUser] = useState(null);
     //const [loading, setLoading] = useState(true);
@@ -41,11 +49,59 @@ export default function recommendationEnginePage() {
     // if (!user) {
     //   return <div>User not found.</div>; // Show a fallback if user data isn't available
     // }
+    const toggleDrawer = (anchor, open) => (event) => {
+      if (event.type === 'keydown' && (event.keyCode === 'Tab' || event.keyCode === 'Shift')) {
+        return;
+      }
+      setState({...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+      <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250}}
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+          {['Adopt', 'Meeting', 'Contact'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {text === 'Adopt' && <PetsIcon />}
+                  {text === 'Meeting' && <GroupsIcon />}
+                  {text === 'Contact' && <ContactsIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
 
     return (
         <main>
             <AppBar position="static">
         <Toolbar>
+          {/* Menu items */}
+          {['Menu'].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <Button 
+              color="inherit" 
+              onClick={toggleDrawer(anchor, true)} 
+              menuIcon={<MenuIcon/>}>
+                {anchor}
+              </Button>
+              <Drawer 
+              anchor={anchor} 
+              open={state[anchor]} 
+              onClose={toggleDrawer(anchor, false)}
+              sx={{ zIndex: (theme) => theme.zIndex.modal + 1}}>
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+          ))}
+
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Whisker Works
           </Typography>
