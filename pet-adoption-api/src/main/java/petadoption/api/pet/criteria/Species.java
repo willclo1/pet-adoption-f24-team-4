10ce50b238d02eb1e24f693f1ee9b52c0cbca660
Rafe Loya <rafe_loya1@baylor.edu>
2024-11-07 11:30:02 -0600

    Update to criteria enums

--- pet-adoption-api/src/main/java/petadoption/api/pet/criteria/Species.java
+++ pet-adoption-api/src/main/java/petadoption/api/pet/criteria/Species.java
@@ -1,9 +1,12 @@
 package petadoption.api.pet.criteria;
 
+import lombok.Getter;
+
 public enum Species {
     DOG("Dog"),
     CAT("Cat");
 
+    @Getter
     private final String displayName;
 
     Species(final String speciesType) {
