12a0a4592590d5724ba816312f07dce6983df612
Rafe Loya <rafe_loya1@baylor.edu>
2024-11-09 01:11:13 -0600

    Added functions for rating calculation & JavaDoc comments

--- pet-adoption-api/src/main/java/petadoption/api/recommendationEngine/RecommendationEngine.java
+++ pet-adoption-api/src/main/java/petadoption/api/recommendationEngine/RecommendationEngine.java
@@ -3,12 +3,49 @@ package petadoption.api.recommendationEngine;
 import lombok.extern.log4j.Log4j2;
 import petadoption.api.pet.Pet;
 import petadoption.api.pet.criteria.FurColor;
+import petadoption.api.pet.criteria.Species;
 import petadoption.api.pet.criteria.Temperament;
 import petadoption.api.pet.criteria.breed.CatBreed;
 import petadoption.api.pet.criteria.breed.DogBreed;
 
+/**
+ * @author Rafe Loya
+ *
+ * @see UserPreferences
+ * @see Pet
+ */
 @Log4j2
 public class RecommendationEngine {
+    /**
+     * Default value used to increment / decrement
+     */
+    public static final double DEFAULT_VAL = 0.001;
+
+    /**
+     * Default value for incrementing after attempting to adopt pet
+     */
+    public static final double DEFAULT_ADOPT_VAL = 0.050;
+
+    /**
+     * Logs the species that caused the error and
+     * throws a new <code>RunTimeException</code>
+     *
+     * @param s <code>Species</code> that caused the error
+     */
+    private void unknownSpeciesError(Species s) throws RuntimeException {
+        log.error("Unknown species encountered: {}", s);
+        throw new RuntimeException("Unknown species encountered: " + s);
+    }
+
+    /**
+     * Determines <code>Breed</code> associated with the pet,
+     * then iterates through each <code>Breed</code>,
+     * summing the values of each associated rating.
+     *
+     * @param up <code>User</code>'s specific preferences
+     * @param p  <code>Pet</code> to calculate ratings from
+     * @return   sum of ratings associated with pet's breeds
+     */
     public double calculateBreedRating(UserPreferences up, Pet p) {
         double rating = 0.0;
 
@@ -24,17 +61,20 @@ public class RecommendationEngine {
                 }
                 break;
             default:
-                log.error(
-                        "Unknown species encountered: {}",
-                        p.getSpecies());
-                throw new RuntimeException(
-                        "Unknown species encountered: " + p.getSpecies()
-                );
+                unknownSpeciesError(p.getSpecies());
         }
 
         return rating;
     }
 
+    /**
+     * Iterates through <code>Pet</code>'s <code>FurColor</code>s,
+     * summing the values of each associated rating.
+     *
+     * @param up <code>User</code>'s specific preferences
+     * @param p  <code>Pet</code> to calculate ratings from
+     * @return   sum of ratings associated with pet's fur colors
+     */
     public double calculateFurColorRating(UserPreferences up, Pet p) {
         double rating = 0.0;
 
@@ -45,6 +85,14 @@ public class RecommendationEngine {
         return rating;
     }
 
+    /**
+     * Iterates through <code>Pet</code>'s <code>Temperament</code>s,
+     * summing the values of each associated rating.
+     *
+     * @param up <code>User</code>'s specific preferences
+     * @param p  <code>Pet</code> to calculate ratings from
+     * @return   sum of ratings associated with pet's temperaments
+     */
     public double calculateTemperamentRating(UserPreferences up, Pet p) {
         double rating = 0.0;
 
@@ -55,6 +103,15 @@ public class RecommendationEngine {
         return rating;
     }
 
+    /**
+     * Calculates a <code>Pet</code>'s total rating through
+     * the members in <Code>UserPreferences</Code>, which map to
+     * the specific criterion's rating.
+     *
+     * @param up <code>User</code>'s specific preferences
+     * @param p  <code>Pet</code> to calculate ratings from
+     * @return   given pet's total rating
+     */
     public double calculatePetRating(UserPreferences up, Pet p) {
         double totalRating = 0.0;
 
@@ -71,5 +128,102 @@ public class RecommendationEngine {
         totalRating += up.getSexRating(p.getSex());                       // Sex
 
         return totalRating;
+    }
+
+    /**
+     * Changes a given <code>User</code>'s <code>UserPreferences</code>
+     * according to a passed <code>double</code>.
+     * <p>
+     * This function is intended to be a helper function for
+     * <code>ratePet()</code></code> and <code>rateAdoptedPet()</code>,
+     * but can be called directly for custom increment / decrement values.
+     *
+     * @param up        <code>User</code>'s specific preferences
+     * @param p         <code>Pet</code> to reference criteria from
+     * @param updateVal value to increment / decrement critera ratings by
+     */
+    public void updatePreferences(UserPreferences up, Pet p, double updateVal) {
+        //double updateVal = like ? DEFAULT_VAL : -(DEFAULT_VAL);
+
+        // Species
+        up.updateSpeciesRating(p.getSpecies(), updateVal);
+
+        // Breed
+        switch(p.getSpecies()) {
+            case CAT:
+                for (CatBreed cb : p.getCatBreed()) {
+                    up.updateCatBreedRating(cb, updateVal);
+                }
+                break;
+            case DOG:
+                for (DogBreed db : p.getDogBreed()) {
+                    up.updateDogBreedRating(db, updateVal);
+                }
+                break;
+            default:
+                unknownSpeciesError(p.getSpecies());
+        }
+
+        // Size
+        up.updateSizeRating(p.getPetSize(), updateVal);
+
+        // Fur Color
+        for (FurColor fc : p.getFurColor()) {
+            up.updateFurColorRating(fc, updateVal);
+        }
+
+        // Fur Type
+        up.updateFurTypeRating(p.getFurType(), updateVal);
+
+        // Coat Length
+        up.updateCoatLengthRating(p.getCoatLength(), updateVal);
+
+        // Age
+        up.updateAgeRating(p.getAge(), updateVal);
+
+        // Temperament
+        for (Temperament t : p.getTemperament()) {
+            up.updateTemperamentRating(t, updateVal);
+        }
+
+        // Health
+        up.updateHealthRating(p.getHealthStatus(), updateVal);
+
+        // Spayed / Neutered
+        up.updateSpayedNeuteredRating(p.getSpayedNeutered(), updateVal);
+
+        // Sex
+        up.updateSexRating(p.getSex(), updateVal);
+    }
+
+    /**
+     * Increments / decrements the ratings in the given <code>UserPreferences</code>
+     * mapped by the criteria contained in a passed <code>Pet</code> instance.
+     *
+     * @param up   <code>User</code>'s specific preferences
+     * @param p    <code>Pet</code> to reference criteria from
+     * @param like if the <code>User</code> liked the pet
+     */
+    public void ratePet(UserPreferences up, Pet p, boolean like) {
+        double updateVal = like ? DEFAULT_VAL : -(DEFAULT_VAL);
+
+        updatePreferences(up, p, updateVal);
+    }
+
+    /**
+     * Increments the ratings in the given <code>UserPreferences</code>
+     * mapped by the criteria contained in a passed <code>Pet</code>
+     * instance. The amount that the criteria ratings will be incremented by
+     * is significantly larger than <code>ratePet()</code>.
+     * <p/>
+     * This function is intended to be used when a <code>User</code>
+     * contacts the <code>AdoptionCenter</code> associated with the given
+     * <code>Pet</code>.
+     *
+     * @param up <code>User</code>'s specific preferences
+     * @param p  <code>Pet</code> to reference criteria from
+     */
+    public void rateAdoptedPet(UserPreferences up, Pet p) {
+        updatePreferences(up, p, DEFAULT_ADOPT_VAL);
     }
 }
