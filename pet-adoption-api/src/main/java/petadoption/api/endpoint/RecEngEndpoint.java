14693966a467d406c35745981e9cce2abdb5139a
Rafe Loya <rafe_loya1@baylor.edu>
2024-11-10 13:46:44 -0600

    Renamed RecommendationEngineEndpoint to RecEngEndpoint

--- pet-adoption-api/src/main/java/petadoption/api/endpoint/RecEngEndpoint.java
+++ pet-adoption-api/src/main/java/petadoption/api/endpoint/RecEngEndpoint.java
@@ -1,16 +1,19 @@
 package petadoption.api.endpoint;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import petadoption.api.user.User;
+import petadoption.api.pet.PetService;
+import petadoption.api.recommendationEngine.RecEngService;
 import petadoption.api.user.UserService;
 
 @Log4j2
 @RestController
-public class RecommendationEngineEndpoint {
+@RequestMapping("/RecEng")
+public class RecEngEndpoint {
+    /*
     @GetMapping("/recommendationEngine")
     public String recEngine_title() {
         return "Welcome to the Recommendation Engine!";
     }
+    */
 }
