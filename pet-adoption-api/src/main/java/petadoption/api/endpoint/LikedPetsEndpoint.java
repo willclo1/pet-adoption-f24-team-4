466e52a6a76f13ef6ad7456d9f0376c3d826fa2b
Rafe Loya <rafe_loya1@baylor.edu>
2024-11-13 15:28:56 -0600

    LikedPetsEndpoint update

--- pet-adoption-api/src/main/java/petadoption/api/endpoint/LikedPetsEndpoint.java
+++ pet-adoption-api/src/main/java/petadoption/api/endpoint/LikedPetsEndpoint.java
@@ -3,23 +3,53 @@ package petadoption.api.endpoint;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import petadoption.api.likedPets.LikedPet;
 import petadoption.api.likedPets.LikedPetService;
 import petadoption.api.pet.Pet;
+import petadoption.api.pet.PetService;
+import petadoption.api.recommendationEngine.RateAdoptedPetBody;
+import petadoption.api.user.UserService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Log4j2
 @RestController
+@RequestMapping("/likedPets")
 public class LikedPetsEndpoint {
     @Getter
     @Autowired
     private LikedPetService likedPetService;
 
-    @GetMapping("/likedPets")
+    @Getter
+    @Autowired
+    private UserService userService;
+
+    @Getter
+    @Autowired
+    private PetService petService;
+
+    @GetMapping("/getPets")
     public List<Pet> getLikedPets(@RequestBody long userId) {
         return likedPetService.getLikedPets(userId);
+    }
+
+    /*
+     * I know it's using a RatedPetBody, dont worry about it :(
+     */
+    @PostMapping("/addPet")
+    public ResponseEntity<LikedPet> addLikedPet(@RequestBody RateAdoptedPetBody body) {
+        try {
+            LikedPet result = likedPetService.saveLikedPets(new LikedPet(
+                    userService.findUser(body.userId).get(),
+                    petService.getPetById(body.petId).get())
+            );
+            return ResponseEntity.ok(result);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
