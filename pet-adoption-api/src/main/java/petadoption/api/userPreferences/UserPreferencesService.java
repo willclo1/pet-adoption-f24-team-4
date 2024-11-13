60d9bb43e3316f041254c9ce588382922892843a
Rafe Loya <rafe_loya1@baylor.edu>
2024-11-12 19:28:18 -0600

    Added deleteAll() functions for UserPreferences testing

--- pet-adoption-api/src/main/java/petadoption/api/userPreferences/UserPreferencesService.java
+++ pet-adoption-api/src/main/java/petadoption/api/userPreferences/UserPreferencesService.java
@@ -25,4 +25,8 @@ public class UserPreferencesService {
     public void deleteUserPreferences(UserPreferences userPreferences) {
         userPreferencesRepository.delete(userPreferences);
     }
+
+    public void deleteAllUserPreferences() {
+        userPreferencesRepository.deleteAll();
+    }
 }
