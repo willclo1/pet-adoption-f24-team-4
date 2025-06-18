a9323b92d67d034e37b16ff7c033d865954cf941
Will Clore <115596953+willclo1@users.noreply.github.com>
2025-06-18 14:38:50 -0500

    Update WebConfig.java

--- pet-adoption-api/src/main/java/petadoption/api/config/WebConfig.java
+++ pet-adoption-api/src/main/java/petadoption/api/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://3.23.241.245:3000")
+                .allowedOrigins("http://localhost:3000", "http://34.132.95.47:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
