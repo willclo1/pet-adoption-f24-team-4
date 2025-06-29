e3eb3d3be46332ea1debd013c54423153f637687
Will Clore <willclore@yahoo.com>
2025-06-28 21:15:21 -0500

    hello

--- pet-adoption-api/src/main/java/petadoption/api/config/WebConfig.java
+++ pet-adoption-api/src/main/java/petadoption/api/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://35.209.179.42:3000")
+                .allowedOrigins("http://localhost:3000", "http://35.209.179.42:3000","http://pet-adopt.online)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
